8bd5d712ae471f2276147c9cd0ff080d63f7d25c
yutaro <136072911+Yuuu12345@users.noreply.github.com>
2023-11-25 21:09:04 +0900

    openaiAPi実装

---
+++ app/opneai_api.py
@@ -0,0 +1,17 @@
+from openai import OpenAI
+
+client=OpenAI(
+  api_key="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"
+)
+messages=[
+    {"role":"user",
+     "content":"調子はどう？",}
+]
+
+response =client.chat.completions.create(
+  messages=messages,
+  model="gpt-4-1106-preview",
+  n=1,
+  max_tokens=100
+)
+print(response)
